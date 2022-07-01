3efc496a9839ba4b3dc949618d464c5c7b2bb9a2
Jonathan Fox <jf162@icloud.com>
2022-07-01 13:30:26 +0100

    changed shorthand to on

--- views/includes/public/js/animation.js
+++ views/includes/public/js/animation.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $(".login_button").click(function() {
+    $(".login_button").on("click", function() {
        $(".form-box").addClass("hidden")
        $(".login-box").removeClass("hidden")
     }) 
-    $(".back_button").click(function() {
+    $(".back_button").on("click", function() {
         $(".login-box").addClass("hidden")
         $(".form-box").removeClass("hidden")
      })
