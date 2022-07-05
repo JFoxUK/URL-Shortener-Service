f3140faad6d9b9fba9bf90bb3fb093c1df0065fb
Jonathan Fox <jf162@icloud.com>
2022-07-05 13:39:42 +0100

    Added conditional logout

--- views/includes/public/js/animation.js
+++ views/includes/public/js/animation.js
@@ -1,10 +1,3 @@
 $(window).on("load", function(){
-    $(".login_button").on("click", function() {
-       $(".form-box").addClass("hidden")
-       $(".login-box").removeClass("hidden")
-    }) 
-    $(".back_button").on("click", function() {
-        $(".login-box").addClass("hidden")
-        $(".form-box").removeClass("hidden")
-     })
+   
 });
