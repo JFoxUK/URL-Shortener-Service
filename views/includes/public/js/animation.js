da23e08f83ce2d462517e8796543da8865b6c3c4
Jonathan Fox <jf162@icloud.com>
2022-07-06 20:25:10 +0100

    updated a<> to be <button>

--- views/includes/public/js/animation.js
+++ views/includes/public/js/animation.js
@@ -1,15 +1,14 @@
-$(window).on("load", function(){
-    $(".tooltip-info").on("click", function() {
-       $(".tooltip-container-info").addClass("hidden")
-       $(".tooltip-container-back").removeClass("hidden")
-       $(".form").addClass("hidden")
-       $(".url-container").removeClass("hidden")
-    }) 
-    $(".tooltip-back").on("click", function() {
-      $(".tooltip-container-back").addClass("hidden")
-      $(".tooltip-container-info").removeClass("hidden")
-      $(".form").removeClass("hidden")
-      $(".url-container").addClass("hidden")
-   }) 
+$(window).on("load", function () {
+	$(".tooltip-info").on("click", function () {
+		$(".tooltip-container-info").addClass("hidden");
+		$(".tooltip-container-back").removeClass("hidden");
+		$(".form").addClass("hidden");
+		$(".url-container").removeClass("hidden");
+	});
+	$(".tooltip-back").on("click", function () {
+		$(".tooltip-container-back").addClass("hidden");
+		$(".tooltip-container-info").removeClass("hidden");
+		$(".form").removeClass("hidden");
+		$(".url-container").addClass("hidden");
+	});
 });
-
