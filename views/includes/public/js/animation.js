01369dac591053b958269b65fb4415f9a0bfbba1
Jonathan Fox <jf162@icloud.com>
2022-07-07 11:44:12 +0100

    hid etoast when clicked on info

--- views/includes/public/js/animation.js
+++ views/includes/public/js/animation.js
@@ -3,12 +3,14 @@ $(window).on("load", function () {
 		$(".tooltip-container-info").addClass("hidden");
 		$(".tooltip-container-back").removeClass("hidden");
 		$(".form").addClass("hidden");
+      $("toast-message").addClass("hidden");
 		$(".url-container").removeClass("hidden");
 	});
 	$(".tooltip-back").on("click", function () {
 		$(".tooltip-container-back").addClass("hidden");
 		$(".tooltip-container-info").removeClass("hidden");
 		$(".form").removeClass("hidden");
+      $("toast-message").removeClass("hidden");
 		$(".url-container").addClass("hidden");
 	});
    $(".copy-button").on("click", function () {
