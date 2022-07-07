ccc2826c4d2ff7946febcb8048fcc49cd3c8f1e7
Jonathan Fox <jf162@icloud.com>
2022-07-07 11:50:29 +0100

    updated remove vlass to target correct element cls

--- views/includes/public/js/animation.js
+++ views/includes/public/js/animation.js
@@ -3,14 +3,18 @@ $(window).on("load", function () {
 		$(".tooltip-container-info").addClass("hidden");
 		$(".tooltip-container-back").removeClass("hidden");
 		$(".form").addClass("hidden");
-      $("toast-message").addClass("hidden");
+      $(".error-message").addClass("hidden");
+      $(".info-short-url-message").addClass("hidden");
+      $(".short-url-message").addClass("hidden");
 		$(".url-container").removeClass("hidden");
 	});
 	$(".tooltip-back").on("click", function () {
 		$(".tooltip-container-back").addClass("hidden");
 		$(".tooltip-container-info").removeClass("hidden");
-		$(".form").removeClass("hidden");
-      $("toast-message").removeClass("hidden");
+		$(".error-message").removeClass("hidden");
+      $(".info-short-url-message").removeClass("hidden");
+      $(".short-url-message").removeClass("hidden");
+      $(".toast-message").removeClass("hidden");
 		$(".url-container").addClass("hidden");
 	});
    $(".copy-button").on("click", function () {
